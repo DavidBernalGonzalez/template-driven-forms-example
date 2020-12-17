c22a030333c2f3dab4470f1ae633d5887dd30c68
David <eldeividlh@gmail.com>
2020-12-17 23:21:59 +0100

    Edito el formulario

---
+++ src/app/my-form/my-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../classes/country';
+import { Person } from '../classes/person';
+
+@Component({
+  selector: 'app-my-form',
+  templateUrl: './my-form.component.html',
+  styleUrls: ['./my-form.component.css']
+})
+export class MyFormComponent implements OnInit {
+  person: Person;
+  country: Country[];
+
+  constructor() {
+    this.person = new Person();
+    this.country = [
+      {id: 1, nombre: 'Spain'},
+      {id: 2, nombre: 'France'},
+      {id: 3, nombre: 'Italia'},
+      {id: 4, nombre: 'Germany'},
+      {id: 5, nombre: 'United Kindom'},
+    ];
+  }
+
+  onSubmitForm(value: any): void{
+    console.log(value);
+  }
+
+  ngOnInit(): void {}
+}
